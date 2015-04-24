8d65751736c66d384e5f1e3b29772b16109eaca1
walid-shalaby <wshalaby@uncc.edu>
2015-04-24 15:14:22 -0400

    another integration iteration

--- src/main/java/edu/uncc/cs/loqr/Associations.java
+++ src/main/java/edu/uncc/cs/loqr/Associations.java
@@ -26,7 +26,7 @@ public class Associations {
 	 */
 	public static List<Rule> associate(Query query, Instances insts) {		
 		// randomly select subset of size 25% of original dataset
-		Instances D  = new Instances(insts);
+		Instances D  = new Instances(insts, insts.numInstances());
 		int toremove = (int)(insts.numInstances()*0.75);
 		Random rand = new Random(100);
 		for(int i=0; i<toremove; i++) {
